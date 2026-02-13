36355d607a89cd914d1cf2b30e14e8ed607ed2b7
Armankabirr <armanutsho12@gmail.com>
2026-02-13 22:42:16 +0600

    added custom carsor

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+
+
+// custom cursar
+var cursor = document.querySelector("#cursor")
+document.addEventListener("mousemove", function(dets) {
+    cursor.style.left = dets.x + "px"
+    cursor.style.top = dets.y + "px"
+})
+
+
 gsap.to("#nav", {
     backgroundColor: "#000",
     height: "70px",
@@ -5,9 +15,21 @@ gsap.to("#nav", {
     scrollTrigger: {
         trigger: "#nav",
         scroller: "body",
-        markers: true,
+        // markers: true,
         start: "top -10%",
         end: "top -11%",
         scrub: 1
     }
 })
+
+gsap.to("#main", {
+    backgroundColor: "#000",
+    scrollTrigger: {
+        trigger: "#main",
+        scroller: "body",
+        start: "top -25%",
+        end: "top -70%",
+        scrub: 2
+    }
+})
+
